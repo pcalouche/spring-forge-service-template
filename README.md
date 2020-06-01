78ae8120482b962d4ccd2bd4cd92a7c536ddbdd3
Philip Calouche <philip.calouche@outlook.com>
2020-05-31 22:42:43 -0400

    update to latest spring elixir and spring boot 2.3.0

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Spring Skeleton is a sample project to showcase the [Spring Elixir Library](https://github.com/pcalouche/springelixir) that uses Spring 
 Auto-configuration to help with the configuration of some things.
 
-Spring Elixir provides a status endpoint, standard controller advice exception handling, and Swagger documentation configured.  Running this application and
-going to [localhost:8080/swagger-ui.html](localhost:8080/swagger-ui.html) will display the Swagger documentation.  From there you'll see two endpoints:
+Spring Elixir provides a status endpoint, standard controller advice exception handling, and Open Api documentation configured.  Running this application and
+going to [localhost:8080/swagger-ui.html](localhost:8080/swagger-ui.html) will display the Open Api documentation.  From there you'll see two endpoints:
 
 1. /status - Demonstrates the status endpoint that is auto-configured by Spring Elixir.
 1. /exception-test - Demonstrates and endpoint that will throw and exception.  The JSON response returned is derived from the code in Spring Elixir.
--- src/main/java/com/pcalouche/springskeleton/controller/ExceptionTestController.java
+++ src/main/java/com/pcalouche/springskeleton/controller/ExceptionTestController.java
@@ -1,11 +1,22 @@
 package com.pcalouche.springskeleton.controller;
 
+import com.pcalouche.springelixir.exception.JsonExceptionResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExceptionTestController {
 
+    @Operation(description = "Demonstrates exception handling provided Spring Elixir's auto configuration.")
+    @ApiResponse(responseCode = "500",
+            description = "If an unexpected runtime exception occurs which it " +
+                    "always will in this example.",
+            content = @Content(schema = @Schema(implementation = JsonExceptionResponse.class))
+    )
     @GetMapping("/exception-test")
     public String exceptionTest() {
         // This will produce json output of the exception as formatted in Standard Controller Advice
