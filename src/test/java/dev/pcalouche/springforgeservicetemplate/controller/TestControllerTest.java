35d96c7037463c3f2d0045b480aa9b661c607e6a
Phil Calouche <philip.calouche@outlook.com>
2025-11-29 22:04:42 -0500

    spring boot 4 updates

--- src/test/java/dev/pcalouche/springforgeservicetemplate/controller/TestControllerTest.java
+++ src/test/java/dev/pcalouche/springforgeservicetemplate/controller/TestControllerTest.java
@@ -6,7 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import dev.pcalouche.springforge.autoconfigure.controlleradvice.CommonControllerAdvice;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
